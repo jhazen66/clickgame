845c58d976285e85f9681ebb61dc5624a4a2326f
Paul Hazen <paul-hazen@live.com>
2014-03-30 21:42:24 -0700

    Fixed build-appx.ps1 and sign-appxpackage.ps1

--- AppX/Build-AppX.ps1
+++ AppX/Build-AppX.ps1
@@ -17,7 +17,7 @@ $env:Path += ";" + (Join-Path ${Env:ProgramFiles(x86)} "Windows Kits\8.1\bin\x64
 
 [xml]$appxManifest = Get-Content "$appxDirectory\AppxManifest.xml";
 $appXDisplayName = $appxManifest.Package.Properties.DisplayName;
-$publisher = $appxManifest.Package.Properties.PublisherDisplayName;
+$publisher = $appxManifest.Package.Identity.Publisher;
 $appXPackageFile = "$buildDirectory\$appxDisplayName.appx";
 
 # Remove any applications that are installed with the same display name (this serves to remove your last build before installing *this* one)
@@ -40,11 +40,11 @@ if ($sign) {
     $keyName = Join-Path $appxDirectory ('keys\{0}-{1}' -f $env:COMPUTERNAME, ($env:UserName -replace '\ ', ''));
 
     # If a root certificate matching the publisher name listed in the AppXManifest file is not found, then create and install it.
-     # Get all the installed certificates under "Trusted People"
+    # Get all the installed certificates under "Trusted People"
     $certs = certutil -store TrustedPeople 2>$1;
 
     if (-not ($certs -imatch "^Issuer:\ $publisher") -or -not (Test-Path "$keyName.pfx")) {
-        Start-Process powershell.exe -verb runas -ArgumentList ("-noexit $appxDirectory\Sign-AppxPackage.ps1 -publisher '{0}' -keyName '{1}'" -f $publisher, $keyName)  -Wait;
+        Start-Process powershell.exe -verb runas -ArgumentList ("$appxDirectory\Sign-AppxPackage.ps1 -publisher '{0}' -keyName '{1}'" -f $publisher, $keyName)  -Wait;
     }    
         
     signtool.exe sign /fd SHA256 /a /f "$keyName.pfx" $appXPackageFile;
@@ -56,6 +56,6 @@ if ($sign) {
     Add-AppxPackage -Register -Path "$buildDirectory\AppxManifest.xml";
 }
 
-$appXPackage = (Get-AppxPackage -Name "$appXDisplayName*");
-
-Invoke-Command { (Get-AppXPackage clickgame | .\Launch-AppxPackage.ps1) }
+Push-Location $appXDirectory;
+(Get-AppXPackage -Name $appXDisplayName | .\Launch-AppxPackage.ps1);
+Pop-Location;
--- AppX/Sign-AppxPackage.ps1
+++ AppX/Sign-AppxPackage.ps1
@@ -12,7 +12,7 @@ Set-Location (split-path -parent $PSCommandPath);
 # Create self signed root certificate with the issuer equal to the one defined in the appxmanifest.xml file:
 makecert -n "$publisher" -r -h 0 -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -m 6 -sv "$keyName.pvk" "$keyName.cer";
 
-certutil -addStore TrustedPeople "$keyName.cer" -f;
+certutil -addStore TrustedPeople "$keyName.cer";
 
 # Convert the pvk certificate file into a pfx file
 pvk2pfx /pvk "$keyName.pvk" /spc "$keyName.cer" /pfx "$keyName.pfx";

2706b3debc7342ab20f1773d1cc1782dfb7e6638
Paul Hazen <paul-hazen@live.com>
2014-03-30 21:49:45 -0700

    Moved launch-appxpackage contents into build-appx.ps1

--- AppX/Build-AppX.ps1
+++ AppX/Build-AppX.ps1
@@ -12,6 +12,54 @@ Param(
     [switch]$sign = $false
 );
 
+
+function Launch-AppXPackage {
+    Param(
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        $appxPackage
+    )
+    $code = @"
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+namespace Win8 {
+    public enum ActivateOptions
+    {
+        None = 0x00000000,  // No flags set
+        DesignMode = 0x00000001,
+        NoErrorUI = 0x00000002,  // Do not show an error dialog if the app fails to activate.                                
+        NoSplashScreen = 0x00000004,  // Do not show the splash screen when activating the app.
+    }
+
+    [ComImport, Guid("2e941141-7f97-4756-ba1d-9decde894a3d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IApplicationActivationManager
+    {
+        // Activates the specified immersive application for the "Launch" contract, passing the provided arguments
+        // string into the application.  Callers can obtain the process Id of the application instance fulfilling this contract.
+        IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+        IntPtr ActivateForFile([In] String appUserModelId, [In] IntPtr /*IShellItemArray* */ itemArray, [In] String verb, [Out] out UInt32 processId);
+        IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+    }
+
+    [ComImport, Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]
+    public class ApplicationActivationManager : IApplicationActivationManager
+    {
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)/*, PreserveSig*/]
+        public extern IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        public extern IntPtr ActivateForFile([In] String appUserModelId, [In] IntPtr /*IShellItemArray* */ itemArray, [In] String verb, [Out] out UInt32 processId);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        public extern IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+    }
+}
+"@
+
+    Add-Type -TypeDefinition $code;
+    $AppLauncher = new-object Win8.ApplicationActivationManager
+
+    $AppLauncher.ActivateApplication(('{0}!App' -f $appxPackage.PackageFamilyName), $null, [Win8.ActivateOptions]::None, [ref]0);
+}
+
 # Add windows sdk to path (if you are running 8.0 or x86, you may need to change this)
 $env:Path += ";" + (Join-Path ${Env:ProgramFiles(x86)} "Windows Kits\8.1\bin\x64\");
 
@@ -56,6 +104,4 @@ if ($sign) {
     Add-AppxPackage -Register -Path "$buildDirectory\AppxManifest.xml";
 }
 
-Push-Location $appXDirectory;
-(Get-AppXPackage -Name $appXDisplayName | .\Launch-AppxPackage.ps1);
-Pop-Location;
+(Get-AppXPackage -Name $appXDisplayName | Launch-AppxPackage);

15b036f2bf741a9ae7ea6f76d265073b58530050
Paul Hazen <paul-hazen@live.com>
2014-03-30 21:06:40 -0700

    Added register-appxpackage.ps1, which registers an appxpackage in place, and launches it.

---
+++ AppX/Register-AppXPackage.ps1
@@ -0,0 +1,14 @@
+﻿Param(
+    [string]$appxDirectory = (split-path -parent $PSCommandPath),
+    [string]$appDirectory = (Join-path (split-path -parent $appxDirectory) 'app'),
+    [string]$buildDirectory = (Join-Path $appxDirectory 'dist')
+);
+
+Copy-Item "$appxDirectory\AppxManifest.xml", "$appDirectory\*" $buildDirectory -Recurse -Force
+
+[xml]$appxManifest = Get-Content "$appxDirectory\AppxManifest.xml";
+$appXDisplayName = $appxManifest.Package.Properties.DisplayName;
+
+Add-AppxPackage -Register -Path "$buildDirectory\AppxManifest.xml";
+
+(Get-AppxPackage -Name $appXDisplayName | .\Launch-AppxPackage.ps1);
